7894c3a2828414ce3cbb86ca5e644dfb83dd07a3
Akshar Saxena <aksharsaxena2003@gmail.com>
2023-08-10 19:46:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'layouts/index.html'))
+})
+
+app.use('/about', (req, res)=>{
+    res.send('About')
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
